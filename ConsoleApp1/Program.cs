3f4cd3a2fdc5115f344cb37e1ac2bcc2197e7f08
jonsi <jonsi@DESKTOP-VI5FVR6>
2022-06-08 19:20:51 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+Logger logger = new Logger();
+
+Calculator calc = new Calculator(logger);
+
+while (true)
+{
+    try
+    {
+        Console.Write("Введите 1-ое слагаемое: ");
+        var a = Console.ReadLine();
+        Console.Write("Введите 2-ое слагаемое: ");
+        var b = Console.ReadLine();
+        calc.Addition(Convert.ToInt32(a), Convert.ToInt32(b));
+    }
+    catch (Exception ex)
+    {
+        logger.Error(ex.Message);
+    }
+}
+
+public interface IAddition
+{
+    void Addition(int a, int b);
+}
+
+public interface ILogger
+{
+    void Event(string message);
+    void Error(string message);
+}
+
+public class Logger : ILogger
+{
+    public void Error(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    public void Event(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Произошла ошибка: {0}", message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+}
+
+public class Calculator : IAddition
+{
+    ILogger logger { get; }
+    public Calculator(ILogger logger)
+    {
+        this.logger = logger;
+    }
+    public void Addition(int a, int b)
+    {
+        logger.Event("Посчитан результат");
+        Console.WriteLine("Сумма чисел: {0}", a + b);
+    }
+}
---
+++ HW10.1/Program.cs
@@ -0,0 +1,78 @@
+﻿//Задание 1
+
+//Создайте консольный мини-калькулятор, который будет подсчитывать сумму двух чисел. 
+//Определите интерфейс для функции сложения числа и реализуйте его.
+//Дополнительно: добавьте конструкцию Try/Catch/Finally для проверки корректности введённого значения.
+
+//Задание 2
+
+//Реализуйте механизм внедрения зависимостей:
+//добавьте в мини-калькулятор логгер, используя материал из скринкаста юнита 10.1.
+//Дополнительно: текст ошибки, выводимый в логгере, окрасьте в красный цвет, а текст события — в синий цвет.
+
+Calculator calculator = new Calculator();
+calculator.number();
+
+interface ICalculator
+{
+    public void Sum(int a, int b);
+}
+
+class Calculator : ICalculator
+{
+    Logger logger = new Logger();
+    
+    public void number()
+    {
+        while (true)
+
+            try
+            {
+                Console.WriteLine("Введите первый операнд");
+                int operand1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\nВведите второй операнд");
+                int operand2 = Convert.ToInt32(Console.ReadLine());
+
+                Sum(operand1, operand2);
+                Console.WriteLine();
+            }
+
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+    }
+
+    public void Sum(int a, int b)
+    {
+        logger.Event("\nПроизведен расчет");
+
+        Console.WriteLine($"{a} + {b} = {a + b}");
+    }
+}
+
+
+public interface ILogger
+{
+    void Event(string message);
+    void Error(string message);
+}
+
+public class Logger : ILogger
+{
+    public void Error(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Произошла ошибка: Введены некоретные данные \n");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    public void Event(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+}
+
